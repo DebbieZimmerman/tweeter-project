720ddf0a46a27e0d513776bced6ff173d1345074
DebbieZimmerman <debbieseuss@gmail.com>
2020-11-06 12:03:14 +0200

    renderer almost complete - need to fix comments

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
+const tweeter = Tweeter()
+const renderer = Renderer()
 
+renderer.renderPosts(tweeter.getPosts())
---
+++ render.js
@@ -0,0 +1,47 @@
+const Renderer = function (){
+    renderPosts = function (posts) {
+        $('#posts').empty()
+        
+        for (post of posts) {
+            let postElement = $(`<div class="post" data-id="${post.id}">${post.text}</div>`)
+            $('#posts').append(postElement)
+            $('#posts').append('<div class="comments">Comments:</div>')
+            for (comment of post.comments) {
+                let commentId = comment.id
+                let commentText = comment.text
+                let commentElement = $(`<div class="comment" data-id="${commentId}">${commentText}</div>`) 
+                $('.comments').append(commentElement)
+            }
+        }
+       
+    } 
+      return {
+        renderPosts
+    }
+}
+
+
+
+
+// const posts = [
+    //     {name: "Debbie", text: "You're the best!"},
+    //     {name: "Aliza", text: "No, you're the best!"}
+    // ]
+    
+    // const render = function () {
+    //     $('#posts-display').empty()
+    
+    //     for (let post of posts) {
+    //         const postElement = $(`<div>${post.name}: ${post.text}</div>`)
+    //         $('#posts-display').append(postElement)
+    //     }
+    // }
+    
+    // $("button").click(function () {
+    //     const n = ($("#inputname").val())
+    //     const t = ($("#inputtext").val())
+    //     posts.push({name: n, text: t}) 
+    //     render()
+    // })
+    
+    // render()
